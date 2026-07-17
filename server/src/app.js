162f2b73dfc743ef391c6c6f614ce54e8b84aa3f
Pavan-kumar-R06 <pavankumar060905@gmail.com>
2026-07-18 01:00:13 +0530

    Initial commit

--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,25 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://trip-planner-git-main-pavankumar060905-8109s-projects.vercel.app",
+  process.env.CLIENT_ORIGIN,
+].filter(Boolean);
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
