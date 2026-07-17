3123f1db02a576a9b22d9a020ed090393a9d4fda
Pavan-kumar-R06 <pavankumar060905@gmail.com>
2026-07-17 23:32:42 +0530

    Initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,8 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
-const app = require("./src/app");
+
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
