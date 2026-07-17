9d0e826555076a68df304c87566245fde57ff6bc
Pavan-kumar-R06 <pavankumar060905@gmail.com>
2026-07-17 23:01:35 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 const app = require("./src/app");
+const connectDB = require("./src/utils/db");
 
-module.exports = app;
+module.exports = async (req, res) => {
+  try {
+    await connectDB();
+    return app(req, res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Database connection failed",
+      error: err.message,
+    });
+  }
+};
